a2b645d30c700b31a53caa77c952132be1ac862a
Rupam-242006 <iamrupambera24@gmail.com>
2026-08-22 14:46:49 +0530

    supabase is working

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Replace these with the actual URL and Key from your Supabase API settings
+
 const supabaseUrl = 'https://qhubqewiybsdgwfiwggx.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFodWJxZXdpeWJzZGd3Zml3Z2d4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODczNzM0MjYsImV4cCI6MjEwMjk0OTQyNn0.Co1R6DTjABNbvWrLoruhaED6ZHa6VJCPNLC9IdkgQ50';
 
